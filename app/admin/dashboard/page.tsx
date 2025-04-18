61d0218ae8b8712c1fe2a12ff41072b683d24fe5
SHAJAN29 <m.j.sandy2916@gmail.com.com>
2025-04-18 09:33:56 +0530

    delete some features

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      {" "}
+      <h1 className="text-5xl illaramAccent">
+        Welcome to Admin Dashboard
+      </h1>{" "}
+    </div>
+  );
+};
+export default AdminDashboard;
---
+++ app/user/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1>User Dashboard</h1>
+      <p>This is the User Dashboard page.</p>
+    </div>
+  );
+};
+export default UserDashboard;
